0437c842ab1e4c3e1d8073e1c64b3dd5c4405c20
SilkeKnossen <silkeknossen@gmail.com>
2018-12-13 15:10:07 +0100

    added comments

--- Light/Light/ViewController.swift
+++ Light/Light/ViewController.swift
@@ -10,19 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Initialize the status of the ligth
     var lightOn = true
     
+    // When the view did load, check for the button to be pressed.
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
+    // When the button is pressed, change status of the ligth.
     @IBAction func buttonPressed(_ sender: Any) {
         lightOn = !lightOn
         updateUI()
     }
     
+    // When the light is on, background is set to white, when the
+    // light is off, background is set to black.
     func updateUI() {
         view.backgroundColor = lightOn ? .white : .black
     }
